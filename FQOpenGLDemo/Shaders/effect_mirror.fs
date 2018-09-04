7b5073d5f69f93ff802909613384dc7d6d5ed42c
VenkeFan <fanqi_0817@live.com>
2018-09-04 16:59:09 +0800

    电击特效

--- FQOpenGLDemo/Shaders/effect_mirror.fs
+++ FQOpenGLDemo/Shaders/effect_mirror.fs
@@ -8,5 +8,11 @@ uniform sampler2D myTexture;
 
 void main()
 {
+    // 正常
     gl_FragColor = texture2D(myTexture, vTexCoord);
+    
+    // 电击效果
+//    vec4 tex = texture2D(myTexture, vTexCoord);
+//    gl_FragColor = vec4(1.0 - tex.rgb, tex.w);
+    
 }
